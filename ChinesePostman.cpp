9d0cc1b8ca053afc8307b56c73c30bf6a4d420e7
nicktallents <nicktallents11@gmail.com>
2014-12-06 19:41:44 -0500

    program shell

--- ChinesePostman.cpp
+++ ChinesePostman.cpp
@@ -1,1 +1,70 @@
 #include "ChinesePostman.h"
+using namespace std;
+
+ChinesePostman::ChinesePostman()
+{
+	solveRan = false;
+}
+
+ChinesePostman::ChinesePostman(string filename)
+{
+	solveRan = false;
+}
+
+void ChinesePostman::solve()
+{
+	;
+}
+
+double ChinesePostman::optimalCost()
+{
+	if (!solveRan) {
+		cout << "You must run solve() before using this function" << endl;
+		return -1;
+	}
+
+	return 0.0;
+}
+
+void ChinesePostman::print()
+{
+	if (!solveRan) {
+		cout << "You must run solve() before using this function" << endl;
+		return;
+	}
+}
+
+void ChinesePostman::addEdge(string lab, int u, int v, double cost)
+{
+	;
+}
+
+void ChinesePostman::findLeastCostPaths()
+{
+	;
+}
+
+void ChinesePostman::checkValid()
+{
+	;
+}
+
+void ChinesePostman::findOddVertices()
+{
+	;
+}
+
+void ChinesePostman::findFeasible()
+{
+	;
+}
+
+bool ChinesePostman::improvements()
+{
+	return false;
+}
+
+int ChinesePostman::findPath(int from, vector<vector<int>>repEd)
+{
+	return 0;
+}
--- ChinesePostman.h
+++ ChinesePostman.h
@@ -1,10 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
 #ifndef CHINESEPOSTMAN_H
 #define CHINESEPOSTMAN_H
+
+const int NONE = -1; // anything < 0
 
 class ChinesePostman
 {
 public:
+	// Constructors
+	ChinesePostman();
+	ChinesePostman(std::string filename);
+
+	// Solve the Chinese Postman Problem
+	void solve();
+	double optimalCost();	// cost of an optimal Chinese Postman Problem
+	void print();	// print results. Must run solve() first.
+
 private:
+	// Private Member Functions
+	void addEdge(std::string lab, int u, int v, double cost);
+	void findLeastCostPaths();									// Floyd-Warshall Algorithm?
+	void checkValid();											// Determines if the graph is valid
+	void findOddVertices();
+	void findFeasible();										// Not sure what this does yet...find a path from one odd node to another?
+																// Maybe the structure of this function changes since we are doing undirected
+	bool improvements();										// Make improvements through cycle cancelling
+	int findPath(int from, std::vector<std::vector<int>>repEd);	// find a path between odd vertices. Used in print()
+
+	// Member variables
+	bool solveRan;											// has the user run solve() yet?
+	int nVertices;											// number of vertices
+	std::vector<int> degree;								// degrees of vertices
+	std::vector<int> odd;									// odd vertices
+	std::vector<std::vector<int>> edges;					// adjacency matrix, counts edges between vertices
+	std::vector<std::vector<std::string>> label;			// lables of arcs (for each vertex pair). A lable is the edge identifier.
+	std::vector<std::vector<int>> repEdge;					// repeated edges in Chinese Postman Problem
+	std::vector<std::vector<double>> cost;					// costs of cheapest edges
+	std::vector<std::vector<std::string>> cheapestLabel;	// labels of cheapest edges
+	std::vector<std::vector<bool>> defined;					// whether path cost is defined between vertices
+	std::vector<std::vector<int>> path;						// spanning tree of the graph
+	double basicCost;										// total cost of traversing each edge once
 };
 
 #endif
